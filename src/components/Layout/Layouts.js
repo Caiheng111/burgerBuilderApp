864141e4aa9917d612a8746d96a410df184f5077
Cai heng <ianyaheng822@gmail.com>
2020-05-31 18:25:29 +1000

    amend the css

--- src/components/Layout/Layouts.js
+++ src/components/Layout/Layouts.js
@@ -1,25 +1,36 @@
-import React,{Fragment} from 'react'
+import React, { Component, Fragment } from 'react';
 import styles from './Layouts.modules.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layouts extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
- const Layouts = (props) => {
-  return (
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
-    <Fragment>
-        <div>
-          {/* hamburger menu */}
-          <Toolbar/>
-          <SideDrawer/>
-          <main className={styles.content}>
-            {props.children}
-          </main>
-        </div>
-    </Fragment>
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
 
-
-  )
+    render () {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
 }
 
 export default Layouts;
